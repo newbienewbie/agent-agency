d7e9f45b938067e251d24eb70be689dd5e954e83
newbienewbie <webbot@sina.com>
2016-11-09 11:04:16 +0800

    把验证代理有效、持久化的工作从爬虫中移除

--- test/xici/crawler.test.js
+++ test/xici/crawler.test.js
@@ -1,3 +1,4 @@
+const assert=require('assert');
 const Crawler=require('../../lib/xici/crawler');
 
 
@@ -8,6 +9,15 @@ describe('测试西刺代理网站的 crawler.js',function(){
     this.timeout(20000);
     it("#crawl()",function(done){
         crawler.crawl()
+            .then(list=>{
+                assert.ok(Array.isArray(list),"爬虫解析的数据必须是Proxy数组");
+                assert.ok(list.length>0,"解析的数组长度必须大于1");
+                list.forEach(i=>{
+                    assert.ok(i.schema,"数组的每个元素必须都有schema属性");
+                    assert.ok(i.ip,"数组的每个元素必须都有ip属性");
+                    assert.ok(i.port,"数组的每个元素必须都有port属性");
+                });
+            })
             .then(done,done)
             .catch(done);
     });
