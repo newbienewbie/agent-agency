161e16ec802d67b0d0e20bf33b57140ce410604c
newbienewbie <webbot@sina.com>
2016-11-09 15:57:43 +0800

    爬虫解析器解析出的端口号应该为整数或者可转化为整数

--- test/hunter/xici/parser.test.js
+++ test/hunter/xici/parser.test.js
@@ -17,6 +17,9 @@ describe('测试西刺代理网站的 parser.js', function () {
                         assert.ok(list[i].schema, "schema 必然非空");
                         assert.ok(list[i].ip, "ip 必然非空");
                         assert.ok(list[i].port, "port 必然非空");
+                        const port=list[i].port;
+                        assert.ok(typeof port == "number","port 必然为数字");
+                        assert.ok(port%1==0, "port 必然为整数");
                     });
                 })
                 .then(done, done)
