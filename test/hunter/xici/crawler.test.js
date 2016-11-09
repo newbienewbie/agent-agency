df727979d6b9abefc5a81afda06dff4a81c84731
newbienewbie <webbot@sina.com>
2016-11-09 22:04:09 +0800

    把Proxy接口类重命名为ProxyInfo避免和ES6的Proxy混淆

--- test/hunter/xici/crawler.test.js
+++ test/hunter/xici/crawler.test.js
@@ -10,7 +10,7 @@ describe('测试西刺代理网站的 crawler.js',function(){
     it("#crawl()",function(done){
         crawler.crawl()
             .then(list=>{
-                assert.ok(Array.isArray(list),"爬虫解析的数据必须是Proxy数组");
+                assert.ok(Array.isArray(list),"爬虫解析的数据必须是ProxyInfo数组");
                 assert.ok(list.length>0,"解析的数组长度必须大于1");
                 list.forEach(i=>{
                     assert.ok(i.schema,"数组的每个元素必须都有schema属性");
--- test/proxy.test.js
+++ test/proxy.test.js
@@ -1,21 +1,21 @@
-const Proxy=require('../lib/proxy');
+const ProxyInfo=require('../lib/proxy');
 const assert=require('assert');
 
 
 describe("测试 proxy.js",function(){
     describe("#toString()",function(){
         it("默认情况下，返回的应该是个本机网址",function (done) {
-            const proxy=new Proxy();
+            const proxy=new ProxyInfo();
             assert.equal(proxy.toString(),"http://localhost:80");
             done();
         });
         it("返回的总应该是相应URL",function (done) {
-            const proxy=new Proxy("https","8.8.8.8","123");
+            const proxy=new ProxyInfo("https","8.8.8.8","123");
             assert.equal(proxy.toString(),"https://8.8.8.8:123");
             done();
         });
         it("返回的总应该是小写的schema",function (done) {
-            const proxy=new Proxy("HTTPS","8.8.8.8","123");
+            const proxy=new ProxyInfo("HTTPS","8.8.8.8","123");
             assert.equal(proxy.toString(),"https://8.8.8.8:123");
             done();
         });
