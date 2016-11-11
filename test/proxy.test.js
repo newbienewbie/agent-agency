9ec2df7a4120906550981eafb22f4d750929198f
newbienewbie <webbot@sina.com>
2016-11-12 07:36:58 +0800

    重构：把ProxyInfo的toString()改为toUrl()

--- test/proxy.test.js
+++ test/proxy.test.js
@@ -3,20 +3,20 @@ const assert=require('assert');
 
 
 describe("测试 proxy.js",function(){
-    describe("#toString()",function(){
+    describe("#toUrl()",function(){
         it("默认情况下，返回的应该是个本机网址",function (done) {
             const proxy=new ProxyInfo();
-            assert.equal(proxy.toString(),"http://localhost:80");
+            assert.equal(proxy.toUrl(),"http://localhost:80");
             done();
         });
         it("返回的总应该是相应URL",function (done) {
             const proxy=new ProxyInfo("https","8.8.8.8","123");
-            assert.equal(proxy.toString(),"https://8.8.8.8:123");
+            assert.equal(proxy.toUrl(),"https://8.8.8.8:123");
             done();
         });
         it("返回的总应该是小写的schema",function (done) {
             const proxy=new ProxyInfo("HTTPS","8.8.8.8","123");
-            assert.equal(proxy.toString(),"https://8.8.8.8:123");
+            assert.equal(proxy.toUrl(),"https://8.8.8.8:123");
             done();
         });
     })
