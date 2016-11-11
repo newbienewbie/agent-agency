2dd2a0f6a4c9bf7017ff770f1eeadbe6031a8f9d
newbienewbie <webbot@sina.com>
2016-11-12 07:24:31 +0800

    将开心代理定向爬虫添集成到定时任务

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const agency=require('./lib/index');
 //定时抓取
 setInterval(agency.hunt('xici'),60*60*1000);
 setInterval(agency.hunt('cz88'),1*60*1000);
+setInterval(agency.hunt('kxdaili'),1*60*1000);
 
 // 定时清理
 agency.clean();
